1f014e6567245def9a9543c2006b6aee47c5c47b
Constantinos Eleftheriou <cesoftworks@gmail.com>
2017-06-11 16:26:27 +0100

    Further DDM analysis for single patient data

--- develop/2017-06-10-first-attempt.py
+++ develop/2017-06-10-first-attempt.py
@@ -3,14 +3,14 @@
 
 # # First Attempt
 # 
-# Initial attempt to fit data to DDM, following the HDDM quick-start tutorial
+# Initial attempt to fit data from a single subject to DDM, following the HDDM tutorial for a single subject
 
 # In[8]:
 
 get_ipython().magic('matplotlib inline')
 
 
-# In[4]:
+# In[14]:
 
 import hddm
 
@@ -18,10 +18,9 @@ data = hddm.load_csv('../data/data_18333.csv')
 
 model = hddm.HDDM(data, depends_on={'v': 'stim'})
 
+model.find_starting_values()
+
 model.sample(2000, burn=20)
-
-model.print_stats()
-
 
 
 # In[9]:
@@ -30,8 +29,35 @@ model.plot_posteriors()
 
 
 # Looks promising!
+# 
+# Let's formally check for convergence using the Gelman-Rubin R statistic
 
-# In[ ]:
+# In[13]:
+
+models = []
+
+# Generate 5 models
+for i in range(5):
+    m = hddm.HDDM(data, depends_on={'v': 'stim'})
+    m.find_starting_values()
+    m.sample(5000, burn=20)
+    models.append(m)
+    
+hddm.analyze.gelman_rubin(models)
 
 
+# Values are close to 1 and not larger than 1.02, therefore models converge successfully!
+
+# In[15]:
+
+model.print_stats()
+
+
+# Compare the posterior for different drift-rate conditions
+
+# In[17]:
+
+v_1, v_2, v_3, v_4 = model.nodes_db.node[['v(1)', 'v(2)', 'v(3)', 'v(4)']]
+
+hddm.analyze.plot_posterior_nodes([v_1, v_2, v_3, v_4])
 
