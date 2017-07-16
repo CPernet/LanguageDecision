80dfab53f70663f38ac5cf1f30b600ded609b94b
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2017-07-16 18:44:14 +0100

    try building combined data ddm

--- develop/2017-07-16-ddm-mixed-data.py
+++ develop/2017-07-16-ddm-mixed-data.py
@@ -13,6 +13,8 @@ get_ipython().magic('cd ..')
 import warnings; warnings.filterwarnings('ignore')
 
 
+# #### Start by parsing the .mat files from the matched controls
+
 # In[2]:
 
 
@@ -25,4 +27,40 @@ matparser.parse_dir(data_dir)
 
 out_dir = "data/controls.csv"
 data_compiler.compile_dir(data_dir, out_dir)
+
+
+# Clean up - remove *nan* entries as these cause hddm to fail
+
+# In[3]:
+
+
+get_ipython().system('cat data/controls.csv | grep -v nan > data/controls_clean.csv ')
+
+
+# Merge patient and matched control data to single .csv file
+# 
+# First, create tagged versions of the csv files - include "tag" column to differentiate between patient and control
+
+# In[36]:
+
+
+get_ipython().system('cat data/patients_clean.csv | sed 1d | awk -v d="patient" -F"," \'BEGIN { OFS = "," } {$5=d; print}\' | tr -d $\'\\r\' > data/patients_tagged.csv')
+get_ipython().system('cat data/controls_clean.csv | sed 1d | awk -v d="control" -F"," \'BEGIN { OFS = "," } {$5=d; print}\' | tr -d $\'\\r\' > data/controls_tagged.csv')
+
+get_ipython().system('echo "response,rt,subj_idx,stim,subj_type" > data/combined_clean.csv')
+get_ipython().system('cat data/patients_tagged.csv >> data/combined_clean.csv; sed 1d data/controls_tagged.csv >> data/combined_clean.csv')
+
+
+# ## Build HDDM model
+
+# In[38]:
+
+
+import hddm
+
+data = hddm.load_csv('data/combined_clean.csv')
+
+model = hddm.HDDM(data, depends_on={'v': 'stim', 'v': 'subj_type'})
+model.find_starting_values()
+model.sample(6000, burn=20)
 
