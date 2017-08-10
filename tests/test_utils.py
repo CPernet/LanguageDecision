6ad29452290419af575051a92cbd84d6c549b411
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2017-08-10 22:14:39 +0100

    add test for convergence checker

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 import unittest
-import utils
+from utils import matparser, data_compiler, model_tools
 
 
 class TestDataCompiler(unittest.TestCase):
@@ -8,3 +8,25 @@ class TestDataCompiler(unittest.TestCase):
 
 class TestMatlabParser(unittest.TestCase):
     pass
+
+
+class TestModelTools(unittest.TestCase):
+    def setUp(self):
+        self.legit_csv = './test_data/legit.csv'
+
+    def test_convergence_success(self):
+        """
+        Confirm that legitimate models successfully converge
+        """
+        legit_models = model_tools.gen_models(self.legit_csv, samples=3000)
+        self.assertTrue(model_tools.check_convergence(legit_models))
+
+    def test_convergence_failure(self):
+        """
+        Confirm that bogus models fail to converge
+        """
+        return
+
+
+if __name__ == '__main__':
+    unittest.main()
