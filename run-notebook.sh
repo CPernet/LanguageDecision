96d9bb05687f4152b649c3ce2316b5468d986a7b
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2017-09-17 17:25:50 +0100

    log to any home

--- run-notebook.sh
+++ run-notebook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source activate lang-dec
-exec jupyter notebook --ip=0.0.0.0 --port=5000 --no-browser --NotebookApp.token="langdec" >> /home/langdec/jupyter.log 
+exec jupyter notebook --ip=0.0.0.0 --port=5000 --no-browser --NotebookApp.token="langdec" >> ~/jupyter.log 
