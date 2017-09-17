661ccfd3fc925639a6bbc952e4460ab3aca34bc2
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2017-09-17 22:04:26 +0100

    fix environment mismatch error

--- vagrant/run-jupyter.sh
+++ vagrant/run-jupyter.sh
@@ -2,5 +2,4 @@
 
 PROJECT_ROOT=/lang_dec
 
-source activate lang-dec || source /root/anaconda3/envs/lang-dec/bin/activate
-jupyter notebook --no-browser --ip=0.0.0.0 --allow-root $PROJECT_ROOT >> /var/log/lang-dec.log
+(source activate lang-dec || source /root/anaconda3/envs/lang-dec/bin/activate) && jupyter notebook --no-browser --ip=0.0.0.0 --allow-root $PROJECT_ROOT >> /var/log/lang-dec.log
