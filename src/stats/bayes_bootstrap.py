dfa18fe8c6dfd80fd4deab3677bdd9e6a40a2cd5
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2018-02-05 15:36:00 +0000

    add function to bootstrap median diffs

--- src/stats/bayes_bootstrap.py
+++ src/stats/bayes_bootstrap.py
@@ -18,3 +18,21 @@ def bayes_bootstrap(observed, stat_function, replications, resample_size):
             'low': stat_function(data_bs) - (1.96 * scipy.std(data_bs))
         }
     }
+
+
+def bayes_bootstrap_median_diffs(sample1, sample2, replications=10000):
+    bs_sample1 = bayes_bootstrap(sample1, scipy.median, replications, len(sample1))['bootstrap_data']
+    bs_sample2 = bayes_bootstrap(sample2, scipy.median, replications, len(sample2))['bootstrap_data']
+    return _compare_bootstraped_medians(bs_sample1, bs_sample2)
+
+
+def _compare_bootstraped_medians(bs_sample1, bs_sample2):
+    median_diffs = [x - y for x in bs_sample1 for y in bs_sample2]
+    return {
+        'median_diffs': median_diffs,
+        'central_tendency': scipy.median(median_diffs),
+        'CIs': {
+            'high':  scipy.stats.mstats.mquantiles(median_diffs, prob=[0.975])[0],
+            'low': scipy.stats.mstats.mquantiles(median_diffs, prob=[0.025])[0]
+        }
+    }
