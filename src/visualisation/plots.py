40a8e83ce25d226285898fb1c917856797889a78
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2018-03-25 16:31:29 +0100

    generalise rt plot

--- src/visualisation/plots.py
+++ src/visualisation/plots.py
@@ -11,21 +11,27 @@ STIMULI = ['SS', 'CP', 'CS', 'US']
 sns.set_style("whitegrid")
 
 
-def plot_median_rts(derivative_path, out_path=None):
+def plot_rts(derivative_path, out_path=None, plot_medians=False):
     subjects = pd.read_csv(derivative_path)
-    stim_rts = subjects.groupby(['stim', 'subj_idx'])['rt'].median()
+    stim_rts = subjects.groupby(['stim', 'subj_idx'])['rt'].median() if plot_medians \
+        else subjects.groupby(['stim'])['rt']
 
     plot = sns.violinplot(
-        data=[stim_rts[stim] for stim in STIMULI],
-        #palette="colorblind"
+        data=[stim_rts[stim] for stim in STIMULI] if plot_medians
+        else [stim_rts.get_group(stim) for stim in STIMULI],
+        # palette="colorblind"
     )
-    plot.set_xticklabels(['SS', 'CP', 'CS', 'US'])
+    plot.set_xticklabels(STIMULI)
     plot.set_xlabel("Condition")
-    plot.set_ylabel("Median Reaction Time (s)")
+
+    y_label = "Median Reaction Time (s)" if plot_medians else "Reaction Time (s)"
+    plot.set_ylabel(y_label)
     if not out_path:
         return plot
 
-    plot.set_title("Subject median reaction time per condition")
+    title = "Subject Median Reaction Time per Condition" if plot_medians \
+            else "Subject Reaction Time per Condition"
+    plot.set_title(title)
     fig = plot.get_figure()
     fig.savefig(out_path)
 
