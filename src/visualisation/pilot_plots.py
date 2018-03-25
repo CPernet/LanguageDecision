0fd2e30bc100bba3e518b7fae612a028fd9123f7
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2018-03-25 17:30:34 +0100

    add accuracy plot

--- src/visualisation/pilot_plots.py
+++ src/visualisation/pilot_plots.py
@@ -8,6 +8,7 @@ from scipy import mean, median
 
 
 STIMULI = ['SS', 'CP', 'CS', 'US']
+TRIALS_PER_STIM = 30
 sns.set_style("whitegrid")
 
 
@@ -23,21 +24,42 @@ def plot_rts(derivative_path, out_path=None, plot_medians=False):
     )
     plot.set_xticklabels(STIMULI)
     plot.set_xlabel("Condition")
-
     y_label = "Median Reaction Time (s)" if plot_medians else "Reaction Time (s)"
     plot.set_ylabel(y_label)
-    if not out_path:
-        return plot
 
-    title = "Subject Median Reaction Time per Condition" if plot_medians \
+    if out_path:
+        title = "Subject Median Reaction Time per Condition" if plot_medians \
             else "Subject Reaction Time per Condition"
-    plot.set_title(title)
-    fig = plot.get_figure()
-    fig.savefig(out_path)
+        plot.set_title(title)
+        fig = plot.get_figure()
+        fig.savefig(out_path)
+    return plot
 
 
 def plot_accuracy(derivative_path, out_path=None):
-    return
+    subjects = pd.read_csv(derivative_path)
+    stim_accs = subjects.groupby(['stim', 'subj_idx']).response.\
+        agg(lambda x: pd.to_numeric(x, errors='coerce').sum() / x.count())
+
+    plot = sns.boxplot(
+        data=[stim_accs[stim] for stim in STIMULI],
+        showfliers=False
+    )
+    sns.swarmplot(
+        data=[stim_accs[stim] for stim in STIMULI],
+        color=".25",
+        ax=plot
+    )
+    plot.set_xticklabels(STIMULI)
+    plot.set_xlabel("Condition")
+    plot.set_ylabel("Accuracy Score")
+    plot.set_ylim(0, 1.01)
+
+    if out_path:
+        plot.set_title("Subject Accuracy Score per Condition")
+        fig = plot.get_figure()
+        fig.savefig(out_path)
+    return plot
 
 
 def plot_dprime(derivative_path, out_path=None):
